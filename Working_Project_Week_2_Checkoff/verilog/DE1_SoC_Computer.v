8e7d77f1c00b1283add8c110d204009047b4bcfa
Demetrios Emmanuel Gavalas <deg273@cornell.edu>
2026-03-19 16:43:40 -0400

    added nonlinear rho controlled by switches using 3 dsps :(

---
+++ Working_Project_Week_2_Checkoff/verilog/DE1_SoC_Computer.v
@@ -0,0 +1,674 @@
+// `include "full_drum.sv"
+
+module DE1_SoC_Computer (
+	////////////////////////////////////
+	// FPGA Pins
+	////////////////////////////////////
+
+	// Clock pins
+	CLOCK_50,
+	CLOCK2_50,
+	CLOCK3_50,
+	CLOCK4_50,
+
+	// ADC
+	ADC_CS_N,
+	ADC_DIN,
+	ADC_DOUT,
+	ADC_SCLK,
+
+	// Audio
+	AUD_ADCDAT,
+	AUD_ADCLRCK,
+	AUD_BCLK,
+	AUD_DACDAT,
+	AUD_DACLRCK,
+	AUD_XCK,
+
+	// SDRAM
+	DRAM_ADDR,
+	DRAM_BA,
+	DRAM_CAS_N,
+	DRAM_CKE,
+	DRAM_CLK,
+	DRAM_CS_N,
+	DRAM_DQ,
+	DRAM_LDQM,
+	DRAM_RAS_N,
+	DRAM_UDQM,
+	DRAM_WE_N,
+
+	// I2C Bus for Configuration of the Audio and Video-In Chips
+	FPGA_I2C_SCLK,
+	FPGA_I2C_SDAT,
+
+	// 40-Pin Headers
+	GPIO_0,
+	GPIO_1,
+	
+	// Seven Segment Displays
+	HEX0,
+	HEX1,
+	HEX2,
+	HEX3,
+	HEX4,
+	HEX5,
+
+	// IR
+	IRDA_RXD,
+	IRDA_TXD,
+
+	// Pushbuttons
+	KEY,
+
+	// LEDs
+	LEDR,
+
+	// PS2 Ports
+	PS2_CLK,
+	PS2_DAT,
+	
+	PS2_CLK2,
+	PS2_DAT2,
+
+	// Slider Switches
+	SW,
+
+	// Video-In
+	TD_CLK27,
+	TD_DATA,
+	TD_HS,
+	TD_RESET_N,
+	TD_VS,
+
+	// VGA
+	VGA_B,
+	VGA_BLANK_N,
+	VGA_CLK,
+	VGA_G,
+	VGA_HS,
+	VGA_R,
+	VGA_SYNC_N,
+	VGA_VS,
+
+	////////////////////////////////////
+	// HPS Pins
+	////////////////////////////////////
+	
+	// DDR3 SDRAM
+	HPS_DDR3_ADDR,
+	HPS_DDR3_BA,
+	HPS_DDR3_CAS_N,
+	HPS_DDR3_CKE,
+	HPS_DDR3_CK_N,
+	HPS_DDR3_CK_P,
+	HPS_DDR3_CS_N,
+	HPS_DDR3_DM,
+	HPS_DDR3_DQ,
+	HPS_DDR3_DQS_N,
+	HPS_DDR3_DQS_P,
+	HPS_DDR3_ODT,
+	HPS_DDR3_RAS_N,
+	HPS_DDR3_RESET_N,
+	HPS_DDR3_RZQ,
+	HPS_DDR3_WE_N,
+
+	// Ethernet
+	HPS_ENET_GTX_CLK,
+	HPS_ENET_INT_N,
+	HPS_ENET_MDC,
+	HPS_ENET_MDIO,
+	HPS_ENET_RX_CLK,
+	HPS_ENET_RX_DATA,
+	HPS_ENET_RX_DV,
+	HPS_ENET_TX_DATA,
+	HPS_ENET_TX_EN,
+
+	// Flash
+	HPS_FLASH_DATA,
+	HPS_FLASH_DCLK,
+	HPS_FLASH_NCSO,
+
+	// Accelerometer
+	HPS_GSENSOR_INT,
+		
+	// General Purpose I/O
+	HPS_GPIO,
+		
+	// I2C
+	HPS_I2C_CONTROL,
+	HPS_I2C1_SCLK,
+	HPS_I2C1_SDAT,
+	HPS_I2C2_SCLK,
+	HPS_I2C2_SDAT,
+
+	// Pushbutton
+	HPS_KEY,
+
+	// LED
+	HPS_LED,
+		
+	// SD Card
+	HPS_SD_CLK,
+	HPS_SD_CMD,
+	HPS_SD_DATA,
+
+	// SPI
+	HPS_SPIM_CLK,
+	HPS_SPIM_MISO,
+	HPS_SPIM_MOSI,
+	HPS_SPIM_SS,
+
+	// UART
+	HPS_UART_RX,
+	HPS_UART_TX,
+
+	// USB
+	HPS_CONV_USB_N,
+	HPS_USB_CLKOUT,
+	HPS_USB_DATA,
+	HPS_USB_DIR,
+	HPS_USB_NXT,
+	HPS_USB_STP
+);
+
+//=======================================================
+//  PARAMETER declarations
+//=======================================================
+
+
+//=======================================================
+//  PORT declarations
+//=======================================================
+
+////////////////////////////////////
+// FPGA Pins
+////////////////////////////////////
+
+// Clock pins
+input						CLOCK_50;
+input						CLOCK2_50;
+input						CLOCK3_50;
+input						CLOCK4_50;
+
+// ADC
+inout						ADC_CS_N;
+output					ADC_DIN;
+input						ADC_DOUT;
+output					ADC_SCLK;
+
+// Audio
+input						AUD_ADCDAT;
+inout						AUD_ADCLRCK;
+inout						AUD_BCLK;
+output					AUD_DACDAT;
+inout						AUD_DACLRCK;
+output					AUD_XCK;
+
+// SDRAM
+output 		[12: 0]	DRAM_ADDR;
+output		[ 1: 0]	DRAM_BA;
+output					DRAM_CAS_N;
+output					DRAM_CKE;
+output					DRAM_CLK;
+output					DRAM_CS_N;
+inout			[15: 0]	DRAM_DQ;
+output					DRAM_LDQM;
+output					DRAM_RAS_N;
+output					DRAM_UDQM;
+output					DRAM_WE_N;
+
+// I2C Bus for Configuration of the Audio and Video-In Chips
+output					FPGA_I2C_SCLK;
+inout						FPGA_I2C_SDAT;
+
+// 40-pin headers
+inout			[35: 0]	GPIO_0;
+inout			[35: 0]	GPIO_1;
+
+// Seven Segment Displays
+output		[ 6: 0]	HEX0;
+output		[ 6: 0]	HEX1;
+output		[ 6: 0]	HEX2;
+output		[ 6: 0]	HEX3;
+output		[ 6: 0]	HEX4;
+output		[ 6: 0]	HEX5;
+
+// IR
+input						IRDA_RXD;
+output					IRDA_TXD;
+
+// Pushbuttons
+input			[ 3: 0]	KEY;
+
+// LEDs
+output		[ 9: 0]	LEDR;
+
+// PS2 Ports
+inout						PS2_CLK;
+inout						PS2_DAT;
+
+inout						PS2_CLK2;
+inout						PS2_DAT2;
+
+// Slider Switches
+input			[ 9: 0]	SW;
+
+// Video-In
+input						TD_CLK27;
+input			[ 7: 0]	TD_DATA;
+input						TD_HS;
+output					TD_RESET_N;
+input						TD_VS;
+
+// VGA
+output		[ 7: 0]	VGA_B;
+output					VGA_BLANK_N;
+output					VGA_CLK;
+output		[ 7: 0]	VGA_G;
+output					VGA_HS;
+output		[ 7: 0]	VGA_R;
+output					VGA_SYNC_N;
+output					VGA_VS;
+
+
+
+////////////////////////////////////
+// HPS Pins
+////////////////////////////////////
+	
+// DDR3 SDRAM
+output		[14: 0]	HPS_DDR3_ADDR;
+output		[ 2: 0]  HPS_DDR3_BA;
+output					HPS_DDR3_CAS_N;
+output					HPS_DDR3_CKE;
+output					HPS_DDR3_CK_N;
+output					HPS_DDR3_CK_P;
+output					HPS_DDR3_CS_N;
+output		[ 3: 0]	HPS_DDR3_DM;
+inout			[31: 0]	HPS_DDR3_DQ;
+inout			[ 3: 0]	HPS_DDR3_DQS_N;
+inout			[ 3: 0]	HPS_DDR3_DQS_P;
+output					HPS_DDR3_ODT;
+output					HPS_DDR3_RAS_N;
+output					HPS_DDR3_RESET_N;
+input						HPS_DDR3_RZQ;
+output					HPS_DDR3_WE_N;
+
+// Ethernet
+output					HPS_ENET_GTX_CLK;
+inout						HPS_ENET_INT_N;
+output					HPS_ENET_MDC;
+inout						HPS_ENET_MDIO;
+input						HPS_ENET_RX_CLK;
+input			[ 3: 0]	HPS_ENET_RX_DATA;
+input						HPS_ENET_RX_DV;
+output		[ 3: 0]	HPS_ENET_TX_DATA;
+output					HPS_ENET_TX_EN;
+
+// Flash
+inout			[ 3: 0]	HPS_FLASH_DATA;
+output					HPS_FLASH_DCLK;
+output					HPS_FLASH_NCSO;
+
+// Accelerometer
+inout						HPS_GSENSOR_INT;
+
+// General Purpose I/O
+inout			[ 1: 0]	HPS_GPIO;
+
+// I2C
+inout						HPS_I2C_CONTROL;
+inout						HPS_I2C1_SCLK;
+inout						HPS_I2C1_SDAT;
+inout						HPS_I2C2_SCLK;
+inout						HPS_I2C2_SDAT;
+
+// Pushbutton
+inout						HPS_KEY;
+
+// LED
+inout						HPS_LED;
+
+// SD Card
+output					HPS_SD_CLK;
+inout						HPS_SD_CMD;
+inout			[ 3: 0]	HPS_SD_DATA;
+
+// SPI
+output					HPS_SPIM_CLK;
+input						HPS_SPIM_MISO;
+output					HPS_SPIM_MOSI;
+inout						HPS_SPIM_SS;
+
+// UART
+input						HPS_UART_RX;
+output					HPS_UART_TX;
+
+// USB
+inout						HPS_CONV_USB_N;
+input						HPS_USB_CLKOUT;
+inout			[ 7: 0]	HPS_USB_DATA;
+input						HPS_USB_DIR;
+input						HPS_USB_NXT;
+output					HPS_USB_STP;
+
+//=======================================================
+//  REG/WIRE declarations
+//=======================================================
+
+wire			[15: 0]	hex3_hex0;
+//wire			[15: 0]	hex5_hex4;
+
+//assign HEX0 = ~hex3_hex0[ 6: 0]; // hex3_hex0[ 6: 0]; 
+//assign HEX1 = ~hex3_hex0[14: 8];
+//assign HEX2 = ~hex3_hex0[22:16];
+//assign HEX3 = ~hex3_hex0[30:24];
+assign HEX4 = 7'b1111111;
+assign HEX5 = 7'b1111111;
+assign HEX3 = 7'b1111111;
+assign HEX2 = 7'b1111111;
+assign HEX1 = 7'b1111111;
+assign HEX0 = 7'b1111111;
+
+//=======================================================
+// Bus controller for AVALON bus-master
+//=======================================================
+// computes drum wave and fills audio FIFO
+
+reg [31:0] bus_addr ; // Avalon address
+// see 
+// ftp://ftp.altera.com/up/pub/Altera_Material/15.1/University_Program_IP_Cores/Audio_Video/Audio.pdf
+// for addresses
+wire [31:0] audio_base_address = 32'h00003040 ;  // Avalon address
+wire [31:0] audio_fifo_address = 32'h00003044 ;  // Avalon address +4 offset
+wire [31:0] audio_left_address = 32'h00003048 ;  // Avalon address +8
+wire [31:0] audio_right_address = 32'h0000304c ;  // Avalon address +12
+reg [3:0] bus_byte_enable ; // four bit byte read/write mask
+reg bus_read  ;       // high when requesting data
+reg bus_write ;      //  high when writing data
+reg [31:0] bus_write_data ; //  data to send to Avalog bus
+wire bus_ack  ;       //  Avalon bus raises this when done
+wire [31:0] bus_read_data ; // data from Avalon bus
+reg [30:0] timer ;
+reg [3:0] state ;
+wire state_clock ;
+
+// current free words in audio interface
+reg [7:0] fifo_space ;
+// debug check of space
+assign LEDR = fifo_space ;
+
+// use 4-byte-wide bus-master	 
+//assign bus_byte_enable = 4'b1111;
+
+// get some signals exposed
+// connect bus master signals to i/o for probes
+assign GPIO_0[0] = bus_write ;
+assign GPIO_0[1] = bus_read ;
+assign GPIO_0[2] = bus_ack ;
+//assign GPIO_0[3] = ??? ;
+
+always @(posedge CLOCK_50) begin //CLOCK_50
+	// reset state machine and read/write controls
+	if (~KEY[0]) begin
+		state <= 0 ;
+		bus_read <= 0 ; // set to one if a read opeation from bus
+		bus_write <= 0 ; // set to one if a write operation to bus
+		timer <= 0;
+		next_sample <= 1'b0;
+	end
+	else begin
+		next_sample <= 1'b0;
+		// timer just for deubgging
+		timer <= timer + 1;
+
+		// set up read FIFO available space
+		if (state==4'd0) begin
+			bus_addr <= audio_fifo_address ;
+			bus_read <= 1'b1 ;
+			bus_byte_enable <= 4'b1111;
+			state <= 4'd1 ; // wait for read ACK
+		end
+
+		// wait for read ACK and read the fifo available
+		// bus ACK is high when data is available
+		if (state==4'd1 && bus_ack==1) begin
+			state <= 4'd2 ; //4'd2
+			// FIFO space is in high byte
+			fifo_space <= (bus_read_data>>24) ;
+			// end the read
+			bus_read <= 1'b0 ;
+		end
+
+		// When there is room in the FIFO
+		// -- compute next drum sample
+		// -- start write to fifo for each channel
+		// -- first the left channel
+		if (state==4'd2 && fifo_space>8'd2) begin //
+			state <= 4'd3;
+			next_sample <= 1'b1 ; // compute next sample
+			bus_write_data <= ({{14{drum_out[17]}}, drum_out} <<< 14);
+			bus_addr <= audio_left_address ;
+			bus_byte_enable <= 4'b1111;
+			bus_write <= 1'b1 ;
+		end
+		// if no space, try again later
+		else if (state==4'd2 && fifo_space<=8'd2) begin
+			state <= 4'b0 ;
+		end
+
+		// detect bus-transaction-complete ACK
+		// for left channel write
+		// You MUST do this check
+		if (state==4'd3 && bus_ack==1) begin
+			state <= 4'd4 ;
+			bus_write <= 0;
+		end
+
+		// -- now the right channel drum sample write
+		if (state==4'd4) begin //
+			state <= 4'd5;
+			bus_write_data <= ({{14{drum_out[17]}}, drum_out} <<< 14);
+			bus_addr <= audio_right_address ;
+			bus_write <= 1'b1 ;
+		end
+
+		// detect bus-transaction-complete ACK
+		// for right channel write
+		// You MUST do this check
+		if (state==4'd5 && bus_ack==1) begin
+			state <= 4'd0 ;
+			bus_write <= 0;
+		end
+	end
+	
+end // always @(posedge state_clock)
+
+
+//=======================================================
+//  Structural coding
+//=======================================================
+
+Computer_System The_System (
+	////////////////////////////////////
+	// FPGA Side
+	////////////////////////////////////
+
+	// Global signals
+	.system_pll_ref_clk_clk					(CLOCK_50),
+	.system_pll_ref_reset_reset			(1'b0),
+	.sdram_clk_clk								(state_clock),
+
+	// AV Config
+	.av_config_SCLK							(FPGA_I2C_SCLK),
+	.av_config_SDAT							(FPGA_I2C_SDAT),
+
+	// Audio Subsystem
+	.audio_pll_ref_clk_clk					(CLOCK3_50),
+	.audio_pll_ref_reset_reset				(1'b0),
+	.audio_clk_clk								(AUD_XCK),
+	.audio_ADCDAT								(AUD_ADCDAT),
+	.audio_ADCLRCK								(AUD_ADCLRCK),
+	.audio_BCLK									(AUD_BCLK),
+	.audio_DACDAT								(AUD_DACDAT),
+	.audio_DACLRCK								(AUD_DACLRCK),
+
+	// bus-master state machine interface
+	.bus_master_audio_external_interface_address     (bus_addr),     
+	.bus_master_audio_external_interface_byte_enable (bus_byte_enable), 
+	.bus_master_audio_external_interface_read        (bus_read),        
+	.bus_master_audio_external_interface_write       (bus_write),       
+	.bus_master_audio_external_interface_write_data  (bus_write_data),  
+	.bus_master_audio_external_interface_acknowledge (bus_ack),                                  
+	.bus_master_audio_external_interface_read_data   (bus_read_data),   
+	
+	
+	////////////////////////////////////
+	// HPS Side
+	////////////////////////////////////
+	// DDR3 SDRAM
+	.memory_mem_a			(HPS_DDR3_ADDR),
+	.memory_mem_ba			(HPS_DDR3_BA),
+	.memory_mem_ck			(HPS_DDR3_CK_P),
+	.memory_mem_ck_n		(HPS_DDR3_CK_N),
+	.memory_mem_cke		(HPS_DDR3_CKE),
+	.memory_mem_cs_n		(HPS_DDR3_CS_N),
+	.memory_mem_ras_n		(HPS_DDR3_RAS_N),
+	.memory_mem_cas_n		(HPS_DDR3_CAS_N),
+	.memory_mem_we_n		(HPS_DDR3_WE_N),
+	.memory_mem_reset_n	(HPS_DDR3_RESET_N),
+	.memory_mem_dq			(HPS_DDR3_DQ),
+	.memory_mem_dqs		(HPS_DDR3_DQS_P),
+	.memory_mem_dqs_n		(HPS_DDR3_DQS_N),
+	.memory_mem_odt		(HPS_DDR3_ODT),
+	.memory_mem_dm			(HPS_DDR3_DM),
+	.memory_oct_rzqin		(HPS_DDR3_RZQ),
+		  
+	// Ethernet
+	.hps_io_hps_io_gpio_inst_GPIO35	(HPS_ENET_INT_N),
+	.hps_io_hps_io_emac1_inst_TX_CLK	(HPS_ENET_GTX_CLK),
+	.hps_io_hps_io_emac1_inst_TXD0	(HPS_ENET_TX_DATA[0]),
+	.hps_io_hps_io_emac1_inst_TXD1	(HPS_ENET_TX_DATA[1]),
+	.hps_io_hps_io_emac1_inst_TXD2	(HPS_ENET_TX_DATA[2]),
+	.hps_io_hps_io_emac1_inst_TXD3	(HPS_ENET_TX_DATA[3]),
+	.hps_io_hps_io_emac1_inst_RXD0	(HPS_ENET_RX_DATA[0]),
+	.hps_io_hps_io_emac1_inst_MDIO	(HPS_ENET_MDIO),
+	.hps_io_hps_io_emac1_inst_MDC		(HPS_ENET_MDC),
+	.hps_io_hps_io_emac1_inst_RX_CTL	(HPS_ENET_RX_DV),
+	.hps_io_hps_io_emac1_inst_TX_CTL	(HPS_ENET_TX_EN),
+	.hps_io_hps_io_emac1_inst_RX_CLK	(HPS_ENET_RX_CLK),
+	.hps_io_hps_io_emac1_inst_RXD1	(HPS_ENET_RX_DATA[1]),
+	.hps_io_hps_io_emac1_inst_RXD2	(HPS_ENET_RX_DATA[2]),
+	.hps_io_hps_io_emac1_inst_RXD3	(HPS_ENET_RX_DATA[3]),
+
+	// Flash
+	.hps_io_hps_io_qspi_inst_IO0	(HPS_FLASH_DATA[0]),
+	.hps_io_hps_io_qspi_inst_IO1	(HPS_FLASH_DATA[1]),
+	.hps_io_hps_io_qspi_inst_IO2	(HPS_FLASH_DATA[2]),
+	.hps_io_hps_io_qspi_inst_IO3	(HPS_FLASH_DATA[3]),
+	.hps_io_hps_io_qspi_inst_SS0	(HPS_FLASH_NCSO),
+	.hps_io_hps_io_qspi_inst_CLK	(HPS_FLASH_DCLK),
+
+	// Accelerometer
+	.hps_io_hps_io_gpio_inst_GPIO61	(HPS_GSENSOR_INT),
+
+	//.adc_sclk                        (ADC_SCLK),
+	//.adc_cs_n                        (ADC_CS_N),
+	//.adc_dout                        (ADC_DOUT),
+	//.adc_din                         (ADC_DIN),
+
+	// General Purpose I/O
+	.hps_io_hps_io_gpio_inst_GPIO40	(HPS_GPIO[0]),
+	.hps_io_hps_io_gpio_inst_GPIO41	(HPS_GPIO[1]),
+
+	// I2C
+	.hps_io_hps_io_gpio_inst_GPIO48	(HPS_I2C_CONTROL),
+	.hps_io_hps_io_i2c0_inst_SDA		(HPS_I2C1_SDAT),
+	.hps_io_hps_io_i2c0_inst_SCL		(HPS_I2C1_SCLK),
+	.hps_io_hps_io_i2c1_inst_SDA		(HPS_I2C2_SDAT),
+	.hps_io_hps_io_i2c1_inst_SCL		(HPS_I2C2_SCLK),
+
+	// Pushbutton
+	.hps_io_hps_io_gpio_inst_GPIO54	(HPS_KEY),
+
+	// LED
+	.hps_io_hps_io_gpio_inst_GPIO53	(HPS_LED),
+
+	// SD Card
+	.hps_io_hps_io_sdio_inst_CMD	(HPS_SD_CMD),
+	.hps_io_hps_io_sdio_inst_D0	(HPS_SD_DATA[0]),
+	.hps_io_hps_io_sdio_inst_D1	(HPS_SD_DATA[1]),
+	.hps_io_hps_io_sdio_inst_CLK	(HPS_SD_CLK),
+	.hps_io_hps_io_sdio_inst_D2	(HPS_SD_DATA[2]),
+	.hps_io_hps_io_sdio_inst_D3	(HPS_SD_DATA[3]),
+
+	// SPI
+	.hps_io_hps_io_spim1_inst_CLK		(HPS_SPIM_CLK),
+	.hps_io_hps_io_spim1_inst_MOSI	(HPS_SPIM_MOSI),
+	.hps_io_hps_io_spim1_inst_MISO	(HPS_SPIM_MISO),
+	.hps_io_hps_io_spim1_inst_SS0		(HPS_SPIM_SS),
+
+	// UART
+	.hps_io_hps_io_uart0_inst_RX	(HPS_UART_RX),
+	.hps_io_hps_io_uart0_inst_TX	(HPS_UART_TX),
+
+	// USB
+	.hps_io_hps_io_gpio_inst_GPIO09	(HPS_CONV_USB_N),
+	.hps_io_hps_io_usb1_inst_D0		(HPS_USB_DATA[0]),
+	.hps_io_hps_io_usb1_inst_D1		(HPS_USB_DATA[1]),
+	.hps_io_hps_io_usb1_inst_D2		(HPS_USB_DATA[2]),
+	.hps_io_hps_io_usb1_inst_D3		(HPS_USB_DATA[3]),
+	.hps_io_hps_io_usb1_inst_D4		(HPS_USB_DATA[4]),
+	.hps_io_hps_io_usb1_inst_D5		(HPS_USB_DATA[5]),
+	.hps_io_hps_io_usb1_inst_D6		(HPS_USB_DATA[6]),
+	.hps_io_hps_io_usb1_inst_D7		(HPS_USB_DATA[7]),
+	.hps_io_hps_io_usb1_inst_CLK		(HPS_USB_CLKOUT),
+	.hps_io_hps_io_usb1_inst_STP		(HPS_USB_STP),
+	.hps_io_hps_io_usb1_inst_DIR		(HPS_USB_DIR),
+	.hps_io_hps_io_usb1_inst_NXT		(HPS_USB_NXT)
+);
+
+
+wire signed [17:0] drum_out;
+reg next_sample;
+wire [6:0] drum_scan_addr;
+assign drum_scan_addr = 7'd0;
+
+// Q1.17 fixed-point constants
+localparam [17:0] RHO_EFF_Q1_17     = 18'd32768; // 1/4
+
+// Switch-controlled G_tension in Q1.17:
+// SW encodes a binary percentage (0..100), where each count is ~1%.
+// 0%  -> 2^-5 (0.03125, Q1.17=4096)
+// 100%-> 2^-3 (0.12500, Q1.17=16384)
+// Values above 100 are saturated to 100.
+localparam [17:0] G_TENSION_MIN_Q1_17 = 18'd4096;
+localparam [17:0] G_TENSION_MAX_Q1_17 = 18'd16384;
+localparam [17:0] G_TENSION_RANGE_Q1_17 = G_TENSION_MAX_Q1_17 - G_TENSION_MIN_Q1_17;
+wire [6:0] g_tension_pct = (SW > 10'd100) ? 7'd100 : SW[6:0];
+wire signed [17:0] g_tension_q1_17 =
+	$signed(G_TENSION_MIN_Q1_17)
+	+ $signed((G_TENSION_RANGE_Q1_17 * g_tension_pct) / 7'd100);
+
+full_drum drum (
+    .clk(CLOCK_50),
+	.rst(~KEY[0]),
+    .next_sample(next_sample),
+    .rho_eff(RHO_EFF_Q1_17),
+	.G_tension(g_tension_q1_17),
+    .center_center_node(drum_out),
+    .done( /*not used*/ ),
+    .all_middle_nodes(/*not used*/),
+	.scan_addr(drum_scan_addr),
+    .scan_data_all( /*not used*/ )
+);
+
+defparam drum.DATA_WIDTH = 18;
+defparam drum.COLUMN_DEPTH = 100;
+defparam drum.NUM_COLUMNS = 100;
+defparam drum.PEAK_INIT = 131071;
+defparam drum.ETA_SHIFT = 13;
+
+endmodule
+////////////////////////////////////////////////#
